8e15726dd7d39908cb495e2db20a02ccd139a56e
Lai27330 <lai871025@gmail.com>
2024-05-31 22:31:48 +0800

    哈哈哈我把腳本抓進去修好囉

--- Assets/Scripts/Shelter/Shelter.cs
+++ Assets/Scripts/Shelter/Shelter.cs
@@ -3,35 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Fusion;
-using Fusion.Addons.Physics;
 
 using DEMO.Manager;
 
 namespace DEMO.DB
 {
-    public class shelter : NetworkBehaviour
+    public class Shelter : NetworkBehaviour
     {
         private ChangeDetector changes;
-        private UIManager uIManager = null;
 
-        [Networked] public int Durability{ get; set; }
+        [Networked] public int Durability { get; set; }
         public int MaxDurability = 100;
 
         [Networked] private TickTimer durabilityTicker { get; set; }
+        [SerializeField] UIManager uIManager;
 
-        public void SetUIManager(UIManager uIManager)
-        {
-            this.uIManager = uIManager;
-        }
         // Start is called before the first frame update
         public override void Spawned()
         {
             changes = GetChangeDetector(ChangeDetector.Source.SimulationState);
-            durabilityTicker = TickTimer.CreateFromSeconds(Runner,0);
-            
+
             if (Object.HasStateAuthority)
             {
                 SetShelterDurability_RPC(MaxDurability);
+                durabilityTicker = TickTimer.CreateFromSeconds(Runner, 1);
+            }
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (Object.HasStateAuthority)
+            {
+                if (durabilityTicker.Expired(Runner))
+                {
+                    DropDurability_RPC();
+                }
             }
         }
 
@@ -41,43 +47,46 @@ namespace DEMO.DB
         public void SetShelterDurability_RPC(int durability)
         {
             Durability = durability;
+            if (uIManager != null)
+            {
+                uIManager.UpdateDurabilitySlider(Durability, MaxDurability);
+            }
         }
 
         [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
         public void DropDurability_RPC()
         {
-            if(durabilityTicker.Expired(Runner))
+            Debug.Log($"Current Durability: {Durability}");
+            if (Durability > 0)
             {
-                if(Durability > 0){
-                    Durability = Durability - 1;
-                }    
+                Durability -= 1;
+                durabilityTicker = TickTimer.CreateFromSeconds(Runner, 1);
             }
-            durabilityTicker = TickTimer.CreateFromSeconds(Runner,1);
-
         }
-
 
         #endregion
 
         #region - OnChanged Events -
 
-           public override void Render()
+        public override void Render()
+        {
+            foreach (var change in changes.DetectChanges(this, out var previousBuffer, out var currentBuffer))
             {
-                foreach (var change in changes.DetectChanges(this, out var previousBuffer, out var currentBuffer))
+                switch (change)
                 {
-                    switch (change)
-                    {
-                        case nameof(Durability):
+                    case nameof(Durability):
+                        if (uIManager != null)
+                        {
                             uIManager.UpdateDurabilitySlider(Durability, MaxDurability);
-                            break;
+                        }
+                        break;
 
-                        case nameof(durabilityTicker):
-                            uIManager.UpdateDurabilitySlider(Durability, MaxDurability);
-                            break;
-                        
-                    }
+                    case nameof(durabilityTicker):
+                        break;
                 }
             }
+        }
+
         #endregion
     }
 }
