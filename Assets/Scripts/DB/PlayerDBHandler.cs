75d18cde9e909120c6efb8f2c9ecb3ba1c4a1c5d
Ting-Ting Chang <ttchaseaa@gmail.com>
2024-06-21 14:57:11 +0800

    add playername and password warning box

--- Assets/Scripts/DB/PlayerDBHandler.cs
+++ Assets/Scripts/DB/PlayerDBHandler.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json.Linq;
 using TMPro;
 
 using DEMO.Manager;
+using WebSocketSharp;
 
 namespace DEMO.DB
 {
@@ -17,7 +18,10 @@ namespace DEMO.DB
         [SerializeField] private TMP_Text playerNameTxt = null;
         [SerializeField] private TMP_Text playerPasswordTxt = null;
         [SerializeField] private PlayerInfo playerInfo = null;
-
+        [SerializeField] private GameObject WarningPanel = null; // Reference to the warning panel
+        [SerializeField] private TMP_Text warningText = null; // Reference to the warning text component
+        [SerializeField] private Button Closebutton = null; // Reference to the close button
+        
         public void Login()
         {
             action = "login";
@@ -34,6 +38,20 @@ namespace DEMO.DB
         {
             SetPlayerName();
             SetPlayerPassword();
+
+            if (playerInfo.Player_name.Length < 5 || playerInfo.Player_name.Length > 12)  //checking playername input
+            {
+                WarningPanel.SetActive(true);
+                warningText.text = "Player's name must be between 5 to 12 letters.";
+                yield break; // Exit the coroutine if validation fails
+            }
+            if (playerInfo.Player_password.IsNullOrEmpty())  //checking password input
+            {
+                WarningPanel.SetActive(true);
+                warningText.text = "Please enter your password.";
+                yield break; // Exit the coroutine if validation fails
+            }
+
 
             formData = new List<IMultipartFormSection>
             {
@@ -84,6 +102,12 @@ namespace DEMO.DB
         public void SetPlayerPassword()
         {
             playerInfo.Player_password = playerPasswordTxt.text.Trim('\u200b');
+        }
+
+        public void ClosingWarningPanel(){
+            playerNameTxt.text = string.Empty;
+            playerPasswordTxt.text = string.Empty;
+            WarningPanel.SetActive(false);
         }
     }
 }
